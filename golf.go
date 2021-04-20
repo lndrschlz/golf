8676c133d8d41f05786cfd4c13d0a098f57cb1b5
jurjevic <jurica.jurjevic@gmail.com>
2021-04-20 11:04:42 +0200

    Initial commit
    Signed-off-by: jurjevic <jurica.jurjevic@gmail.com>

---
+++ golf.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/traefik/yaegi/interp"
+	"github.com/traefik/yaegi/stdlib"
+	"reflect"
+)
+
+type golf struct {
+	interpreter *interp.Interpreter
+}
+
+func NewGolf(gopath string) *golf {
+	interpreter := interp.New(interp.Options{
+		GoPath: gopath,
+	})
+	interpreter.Use(stdlib.Symbols)
+	// default imports
+	doDotImport(interpreter, "fmt")
+	doDotImport(interpreter, "strings")
+	doImport(interpreter, "os")
+	builtIns(interpreter)
+	return &golf{interpreter: interpreter}
+}
+
+func builtIns(interpreter *interp.Interpreter) {
+	statements := []string{
+		"var arg = make(map[string]interface{})",
+		"func isSet(key string) bool { _, ok := arg[key]; return ok }",
+	}
+	for _, statement := range statements {
+		_, err := interpreter.Eval(statement)
+		checkFail(err, "Failed to initialize the built-in variables and function. Detail: "+statement)
+	}
+}
+
+func doImport(interpreter *interp.Interpreter, pkg string) {
+	_, err := interpreter.Eval(`import "` + pkg + `"`)
+	checkFail(err, "Package not found: "+pkg)
+}
+
+func doDotImport(interpreter *interp.Interpreter, pkg string) {
+	_, err := interpreter.Eval(`import . "` + pkg + `"`)
+	checkFail(err, "Package not found: "+pkg)
+}
+
+func (g *golf) eval(call string, line string) (result string, ok bool, repeat bool, next int64, err error) {
+	// disable the repeat instruction
+	_, err = g.interpreter.Eval("repeat := false")
+	if err != nil {
+		return "", false, false, 0, err
+	}
+	_, err = g.interpreter.Eval("next := 0")
+	if err != nil {
+		return "", false, false, 0, err
+	}
+	_, err = g.interpreter.Eval("line := `" + line + "`")
+	if err != nil {
+		return "", false, false, 0, err
+	}
+	_, err = g.interpreter.Eval("token := Split(line, ` `)")
+	if err != nil {
+		return "", false, false, 0, err
+	}
+	resultValue, err := g.interpreter.Eval(call)
+	result = resultValue.String()
+	ok = resultValue.Kind() == reflect.String
+	if err != nil {
+		return "", false, false, 0, err
+	}
+	repeatValue, err := g.interpreter.Eval("repeat")
+	repeat = repeatValue.Bool()
+	nextValue, err := g.interpreter.Eval("next")
+	next = nextValue.Int()
+	return
+}
